3adeb8930aff829f38b1178b2527c5a5acdc8e41
Aubrey16 <Aubrey.Rayhan.Firoz@gmail.com>
2026-09-25 04:56:58 +0700

    Update : dependency material card view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,13 @@ android {
 dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.gridlayout)
     implementation(libs.androidx.recyclerview)
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
